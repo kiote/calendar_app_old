acffddc44fb920db1a5d684b55ebf3161a1ce433
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-08-07 09:52:42 +0300

    templated event added

--- hello.py
+++ hello.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World!'
+    return '<html><body><a target="_blank" href="https://www.google.com/calendar/event?action=TEMPLATE&tmeid=OXA1bDFmMzl1azlvczB0bXNkdjg4OTU1c28ga3JpdmljaC5la2F0ZXJpbmFAbQ&tmsrc=krivich.ekaterina%40gmail.com"><img border="0" src="https://www.google.com/calendar/images/ext/gc_button1_en.gif"></a></body></html>'
