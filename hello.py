b7d9d35a841f2476462094c05a99104be321bd0f
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-08-10 18:27:10 +0300

    Use calendar, not drive

--- hello.py
+++ hello.py
@@ -28,7 +28,7 @@ def index():
 def oauth2callback():
   flow = client.flow_from_clientsecrets(
       'client_secrets.json',
-      scope='https://www.googleapis.com/auth/drive.metadata.readonly',
+      scope='https://www.googleapis.com/auth/calendar',
       redirect_uri='https://gcalendar-api-events.herokuapp.com/oauth2callback')
 
   auth_uri = flow.step1_get_authorize_url()
