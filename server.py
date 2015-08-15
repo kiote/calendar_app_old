d2960dfbc7c4390c5e74f4e25c79eaa7b5640fbd
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-08-15 10:47:01 +0300

    Catch only general error

--- server.py
+++ server.py
@@ -28,16 +28,8 @@ def index():
         http_auth = credentials.authorize(httplib2.Http())
         service = discovery.build('calendar', 'v3', http=http_auth)
         user_info_service = discovery.build(serviceName='oauth2', version='v2', http=http_auth)
-
-        try:
-            user_info = user_info_service.userinfo().get().execute()
-        except errors.HttpError, e:
-            logging.error('An error occurred: %s', e)
-
-        try:
-            event_created = service.events().insert(calendarId='primary', body=event).execute()
-        except errors.HttpError, e:
-            logging.error('An error occurred: %s', e)
+        user_info = user_info_service.userinfo().get().execute()
+        event_created = service.events().insert(calendarId='primary', body=event).execute()
         return render_template('event.html',
                                event_url=event_created.get('htmlLink'),
                                email=user_info['email'])
