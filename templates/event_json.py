1e01f96822fa45b5bc78064505f8cb978503f501
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-09-03 16:26:20 +0300

    utc for timezone added for event 1

--- templates/event_json.py
+++ templates/event_json.py
@@ -3,10 +3,10 @@ events_json = [{
   'location': 'http://www.path.to/study/website',
   'description': 'A chance to hear more about Google\'s developer products.',
   'start': {
-    'dateTime': '2015-09-28T06:00:00'
+    'dateTime': '2015-09-28T06:00:00Z'
   },
   'end': {
-    'dateTime': '2015-09-28T06:30:00'
+    'dateTime': '2015-09-28T06:30:00Z'
   },
   'attendees': [
     {'email': 'study@studywebsite.com'}
